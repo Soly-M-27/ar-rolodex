fa6f1db7d42a5d0acb989076cad868fbac4e748d
lucas sproule <lucas.sproule.42@gmail.com>
2024-03-31 11:28:42 -0400

    started create page but didn't finish

---
+++ src/components/DropDown.tsx
@@ -0,0 +1,49 @@
+import {Dispatch, SetStateAction, ChangeEvent} from "react";
+type DropDownInputProps = {
+  value: string;
+  setSelectedValues: Dispatch<SetStateAction<string[]>>;
+}
+export const DropDownInput = ({ value, setSelectedValues }: DropDownInputProps) => {
+  const HandleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setSelectedValues((prev) => [...prev, e.target.value]); } else {
+      setSelectedValues((prev) => prev.filter((value) => value !== e.target.value));
+    }
+  }
+  return (
+    <li>
+      <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+        <input onChange={HandleChange}  id={`checkbox-item-${value}`} type="checkbox" value={`${value}`} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+        <label className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">{value}</label>
+      </div>
+    </li>
+  )
+}
+
+type DropDownProps = {
+  dropDown: boolean;
+  setDropDown: React.Dispatch<React.SetStateAction<boolean>>;
+  values?: string[];
+  setSelectedValues: Dispatch<SetStateAction<string[]>>;
+}
+
+export const DropDown = ({ dropDown, setDropDown, values,  setSelectedValues }: DropDownProps) => {
+  return (
+    <>
+      <div className="mb-5">
+        <button
+          type="button"
+          className="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          onClick={() => setDropDown((prev) => !prev)}
+        >add socials</button>
+        <div id="dropdownBgHover" className={`z-10 ${dropDown ? "" : "hidden"} mb-2 rounded-lg shadow dark:bg-gray-700`}>
+          <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownBgHoverButton">
+            {values?.map((value) => (
+              <DropDownInput value={value} setSelectedValues={setSelectedValues}  />
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,29 +1,23 @@
+import { useState } from "react";
+import { Input, DropDown } from "../components";
 
 type Props = {}
 
 export function Create({ }: Props) {
+  const [dropDown, setDropDown] = useState(false);
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
   return (
     <div className="mt-20">
       <form className="max-w-sm mx-auto">
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-          <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required />
-        </div>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Business Name</label>
-          <input type="text" id="Business name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Business Name" required />
-        </div>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone Number</label>
-          <input type="tel" id="Phone Numebr" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Phone Number" required />
-        </div>
-        <div className="mb-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Link Tree</label>
-          <input type="url" id="Link Tree Link" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Link Tree" required />
-        </div>
-
-
-
+        <Input label="Name" type="text" id="Name" placeholder="Name" />
+        <Input label="Business Name" type="text" id="Business name" placeholder="Business Name" />
+        <Input label="Business Email" type="email" id="Business Email" placeholder="Business Email" />
+        <Input label="Phone Number" type="tel" id="Phone Numebr" placeholder="Phone Number" />
+        <Input label="Link Tree" type="url" id="Link Tree" placeholder="Link Tree" />
+        <DropDown dropDown={dropDown} setDropDown={setDropDown} values={["Github", "LinkedIn", "Facebook", "Instagram"]} setSelectedValues={setSelectedValues} />
+        {selectedValues.map((value) => (
+          <Input key={value} label={value} type="url" id={value} placeholder={value} />
+        ))}
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
     </div>
