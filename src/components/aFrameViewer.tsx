99318fbd5f3d400a83f0c34808d7fdc77c7e680e
lucas sproule <lucas.sproule.42@gmail.com>
2024-03-31 22:29:29 -0400

    started create page but didn't finish

--- src/components/aFrameViewer.tsx
+++ src/components/aFrameViewer.tsx
@@ -1,15 +1,17 @@
 import "aframe";
 import "mind-ar";
-//@ts-ignore
-import { Scene } from "aframe-react";
-import { useRef, useEffect } from "react";
+import { Scene, Entity } from "aframe-react";
 
 type Props = {};
 
-export function AFrameViewer({}: Props) {
+export function AFrameViewer({ }: Props) {
   return (
-  <Scene>
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+        <Entity particle-system={{preset: 'snow'}}/>
+        <Entity light={{type: 'point'}}/>
+        <Entity text={{value: 'Hello, WebVR!'}}/>
+      </Scene>
 
-  </Scene>
   );
 }
