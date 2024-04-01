a5b41b3909955b8ecf582bda3afaa9bff048dda7
lucas sproule <lucas.sproule.42@gmail.com>
2024-04-01 19:04:22 -0400

    solymar debug my s**t plz

--- src/components/aFrameViewer.tsx
+++ src/components/aFrameViewer.tsx
@@ -3,9 +3,7 @@ import React, { useState } from "react";
 
 export function AFrameViewer({data}) {
 
-  const [show, setShow] = useState(false);
-  if (!show) {
-    return <button onClick={() => setShow(true)}>Show AR</button>;
-  }
-  return <a-scene></a-scene>;
+  console.log("data",data);
+
+  return <div><a-scene></a-scene></div>;
 }
