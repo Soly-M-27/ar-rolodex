af16226dab740d4aaccf5077bb9e6ed4bbdf3f9c
lucas sproule <lucas.sproule.42@gmail.com>
2024-04-02 16:04:03 -0400

    adding ref

--- src/components/aFrameViewer.tsx
+++ src/components/aFrameViewer.tsx
@@ -1,61 +1,37 @@
 //@ts-nocheck
-import React, { useEffect, useRef } from "react";
-import "aframe";
-import "mind-ar/dist/mindar-image-aframe.prod.js";
+import React, { useEffect, useRef } from 'react';
+import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+import * as THREE from 'three';
 
-export const AFrameViewer = ({ data }) => {
-  const sceneRef = useRef(null);
+export default () => {
+  const containerRef = useRef(null);
 
   useEffect(() => {
-    const sceneEl = sceneRef.current;
-    const arSystem = sceneEl.systems["mindar-image-system"];
-    sceneEl.addEventListener("renderstart", () => {
-      arSystem.start(); // start AR
+    const mindarThree = new MindARThree({
+      container: containerRef.current,
+      imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind"
     });
+    const {renderer, scene, camera} = mindarThree;
+    const anchor = mindarThree.addAnchor(0);
+    const geometry = new THREE.PlaneGeometry(1, 0.55);
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+    const plane = new THREE.Mesh( geometry, material );
+    anchor.group.add(plane);
+
+    mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+
+    return () => {
+      renderer.setAnimationLoop(null);
+      mindarThree.stop();
+    }
   }, []);
 
   return (
-    <div className="absolute mt-48 w-full">
-      <a-scene
-        ref={sceneRef}
-        mindar-image="imageTargetSrc: https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/band.mind;"
-        color-space="sRGB"
-        renderer="colorManagement: true, physicallyCorrectLights"
-        vr-mode-ui="enabled: false"
-        device-orientation-permission-ui="enabled: false"
-      >
-        <a-assets>
-          <a-asset-item
-            id="bearModel"
-            src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/bear/scene.gltf"
-          ></a-asset-item>
-          <a-asset-item
-            id="raccoonModel"
-            src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.5/examples/image-tracking/assets/band-example/raccoon/scene.gltf"
-          ></a-asset-item>
-        </a-assets>
-
-        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-
-        <a-entity mindar-image-target="targetIndex: 0">
-          <a-gltf-model
-            rotation="0 0 0 "
-            position="0 -0.25 0"
-            scale="0.05 0.05 0.05"
-            src="#raccoonModel"
-            animation-mixer
-          />
-        </a-entity>
-        <a-entity mindar-image-target="targetIndex: 1">
-          <a-gltf-model
-            rotation="0 0 0 "
-            position="0 -0.25 0"
-            scale="0.05 0.05 0.05"
-            src="#bearModel"
-            animation-mixer
-          />
-        </a-entity>
-      </a-scene>
+    <div style={{width: "100%", height: "100%"}} ref={containerRef}>
     </div>
-  );
-};
+  )
+}
+
