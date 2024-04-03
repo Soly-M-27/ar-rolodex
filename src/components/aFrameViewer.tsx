ae6dac6abe6bec9fa4edd2a82023a9e7e6f0ef8a
lucas sproule <lucas.sproule.42@gmail.com>
2024-04-02 22:26:35 -0400

    guess who did it? damn right

--- src/components/aFrameViewer.tsx
+++ src/components/aFrameViewer.tsx
@@ -1,42 +1,7 @@
-//@ts-nocheck
-//import React, { useEffect, useRef } from "react";
-import * as THREE from 'three';
 
-import { iFrame } from "./iFrame";
-
-export const AFrameViewer = () => {
+export function AFrameViewer({data}:any){
+  console.log(data);
   return (
-    <div style={{ width: "100%", height: "100%" }} ref={containerRef}>
-      <div dangerouslySetInnerHTML={{__html: iFrame()}}/>
-    </div>
-  );
-  //const containerRef = useRef(null);
-
-  /*useEffect(() => {
-    const mindarThree = new MindARThree({
-      container: containerRef.current,
-      imageTargetSrc:
-        "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind",
-    });
-    const { renderer, scene, camera } = mindarThree;
-    const anchor = mindarThree.addAnchor(0);
-    const geometry = new THREE.PlaneGeometry(1, 0.55);
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x00ffff,
-      transparent: true,
-      opacity: 0.5,
-    });
-    const plane = new THREE.Mesh(geometry, material);
-    anchor.group.add(plane);
-
-    mindarThree.start();
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-    });
-
-    return () => {
-      renderer.setAnimationLoop(null);
-      mindarThree.stop();
-    };
-  }, []);*/
+    <iframe className="w-full h-screen" src={"./src/components/index.html?user=id&card=id"} allowFullScreen={true}></iframe>
+  )
 };
